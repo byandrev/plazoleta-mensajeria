eae92eeab5a64fe5f074a00bec5e09ab8911cb13
Andres Parra <byandrev@gmail.com>
2025-12-13 16:00:08 -0500

    feat(application): enhance MessageRequestDto with validation and builder pattern

--- src/main/java/com/pragma/powerup/application/dto/request/MessageRequestDto.java
+++ src/main/java/com/pragma/powerup/application/dto/request/MessageRequestDto.java
@@ -1,13 +1,20 @@
 package com.pragma.powerup.application.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
+@Builder
 public class MessageRequestDto {
 
     @NotBlank(message = "El 'to' no puede estar vacio")
+    @Pattern(
+            regexp = "^(\\+\\d{1,12}|\\d{1,13})$",
+            message = "Teléfono inválido. Máx 13 caracteres y puede iniciar con +"
+    )
     private String to;
 
     @NotBlank(message = "El 'message' no puede estar vacio")
